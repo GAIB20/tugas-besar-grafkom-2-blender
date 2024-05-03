700f9ae6bb9352c7d05ac0e607b0dd897b360afb
liviaarumsari <13521094@mahasiswa.itb.ac.id>
2024-05-04 04:42:04 +0700

    feat: node base class

--- src/src/libs/vector3.ts
+++ src/src/libs/vector3.ts
@@ -1,4 +1,3 @@
-
 export class Vector3 {
     constructor(public x: number = 0, public y: number = 0, public z: number = 0) {
         this[0] = x;
@@ -11,5 +10,9 @@ export class Vector3 {
 
     get length(): number {
         return 3;
+    }
+
+    isEqual(vector3: Vector3) {
+        return this[0] === vector3[0] && this[1] === vector3[1] && this[2] === vector3[2]
     }
 }
